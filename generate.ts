5bf85190995e34afaa0d94888c2d26ec46e23641
nuanyang233 <nuanyang233@gmail.com>
2023-04-27 16:41:22 +0800

    feat: claimers scripts

---
+++ generate.ts
@@ -0,0 +1,28 @@
+import Claimers from "./claimer.json";
+import { MerkleTree } from "merkletreejs";
+import { utils } from "ethers";
+import { resolve } from "path";
+import { writeFile, mkdir } from "fs/promises";
+
+async function main() {
+  const airdropList = Object.entries(Claimers).map(([address, amount]) => {
+    return utils.keccak256(
+      utils.solidityPack(
+        ["address", "uint256"],
+        [address, utils.parseEther(amount)]
+      )
+    );
+  });
+
+  const merkleTree = new MerkleTree(airdropList, utils.keccak256);
+  const merkleRoot = "0x" + merkleTree.getRoot().toString("hex");
+
+  const pathToDist = resolve(__dirname, "./dist/");
+
+  await mkdir(pathToDist, { recursive: true });
+  await writeFile(`${pathToDist}/merkleRoot.txt`, merkleRoot);
+
+  console.log("Generated merkle root successfully!");
+}
+
+main();
